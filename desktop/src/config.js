3dbdb0e4578b46e656f524e4066e161b2f93e384
Yurii Timofeyev <georgius@ural.ru>
2019-04-23 13:31:08 +0500

    Desktop config due to hosting

--- desktop/src/config.js
+++ desktop/src/config.js
@@ -1,14 +1,12 @@
 const development = {
   apiEndPoint: 'https://127.0.0.1:3500',
   rtcEndPoint: 'wss://127.0.0.1:3500',
-  public: 'http://localhost:3000',
   logLevel: 'debug'
 }
 
 const production = {
-  apiEndPoint: 'https://' + window.location.host,
-  rtcEndPoint: 'wss://' + window.location.host,
-  public: 'https://' + window.location.host,
+  apiEndPoint: 'https://expert.vep.ru',
+  rtcEndPoint: 'wss://expert.vep.ru',
   logLevel: 'warn'
 }
 
