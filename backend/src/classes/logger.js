509dafad9ea9473b0c41c4b91cb10ac484407526
Yurii Timofeyev <georgius@ural.ru>
2019-04-23 14:25:29 +0500

    Prod updates

--- backend/src/classes/logger.js
+++ backend/src/classes/logger.js
@@ -3,9 +3,7 @@
  */
 const options = {
   level: process.env.NODE_ENV === 'production' ? 'info' : 'trace',
-}
-if (process.env.NODE_ENV !== 'production') {
-  options.prettyPrint = { colorize: true }
+  prettyPrint: { colorize: true }
 }
 const Logger = require('pino')(options)
 module.exports = Logger
