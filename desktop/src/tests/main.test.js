5d3d76e285e94969407b0c43cb64a15ca9e661a9
Yurii Timofeyev <georgius@ural.ru>
2019-04-27 22:34:01 +0500

    Tests desktop

--- desktop/src/tests/main.test.js
+++ desktop/src/tests/main.test.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
+import { Provider } from 'react-redux'
+import store from '../store'
 import { MemoryRouter } from 'react-router-dom'
 import View from '../views/main'
 describe('Main view', () => {
   test('it matches the snapshot', () => {
     const view = renderer.create(
-      <MemoryRouter>
-        <View />
-      </MemoryRouter>
+      <Provider store={store}>
+        <MemoryRouter>
+          <View />
+        </MemoryRouter>
+      </Provider>
     )
     expect(view.toJSON()).toMatchSnapshot()
   })
--- desktop/src/tests/register.test.js
+++ desktop/src/tests/register.test.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import store from '../store'
 import renderer from 'react-test-renderer'
 import { MemoryRouter } from 'react-router-dom'
 import View from '../views/register'
 describe('Register view', () => {
   test('it matches the snapshot', () => {
     const view = renderer.create(
-      <MemoryRouter>
-        <View />
-      </MemoryRouter>
+      <Provider store={store}>
+        <MemoryRouter>
+          <View />
+        </MemoryRouter>
+      </Provider>
     )
     expect(view.toJSON()).toMatchSnapshot()
   })
